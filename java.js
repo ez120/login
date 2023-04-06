b63f0a93e7b88f47e91e5c86dbd161bb27b1f53d
ezaldeen <ezsaka55@gmail.com>
2023-04-06 07:56:20 +0300

    up to responsev

--- java.js
+++ java.js
@@ -1,11 +1,19 @@
 let closeSpan =document.querySelector(".span-close");
 let clicks =document.querySelector(".click");
+let icons =document.querySelector(".icons");
+let navs =document.querySelector(".navs");
 let wra =document.querySelector(".wra");
 let reg =document.querySelector(".reg");
 let log =document.querySelector(".log");
 let register =document.querySelector(".register");
 let login =document.querySelector(".login");
 
+icons.onclick =()=>{
+navs.classList.toggle("live");
+}
+// icons.addEventListener("click",()=>{
+// navs.style.cssText ="transform: scale(1);"
+// })
 clicks.addEventListener("click",()=>{
 wra.style.cssText ="transform: scale(1);"
 })
